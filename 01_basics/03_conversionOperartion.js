2c68f6b1b46e984431975a80a9d3933283fb2cc2
AJIT SINGH <96468828+ajit1028@users.noreply.github.com>
2024-07-12 09:11:21 +0000

    conversation operation

---
+++ 01_basics/03_conversionOperartion.js
@@ -0,0 +1,58 @@
+let score =33
+// console.log(typeof score)  //number
+
+let val ="33"
+// console.log(typeof val);// string
+
+let valueInNumber = Number(val);
+//console.log(typeof valueInNumber);//number
+
+
+let val1 = "33abc"
+//console.log(typeof val1);//string
+let val2 = Number(val1);
+//console.log(val2)//NaN
+//console.log(typeof val2);//number
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false =>0
+
+let var3 = null;
+let var4 = Number(var3);
+//console.log(var4);//0
+
+let val5 = undefined;
+let val6 = Number(val5);
+console.log(val6);//NaN
+
+
+
+
+
+let name = "hitesh";
+//console.log(typeof name);//string
+let change = Number(name);
+//console.log(typeof change);//number
+
+
+
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);//
+console.log(typeof stringNumber);
+
+
+
+ 
