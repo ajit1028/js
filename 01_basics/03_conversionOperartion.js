af6cb6ac329148503d1bf30d02788a6d34ce7650
AJIT SINGH <96468828+ajit1028@users.noreply.github.com>
2024-07-12 09:18:14 +0000

    datatypes and operation

--- 01_basics/03_conversionOperartion.js
+++ 01_basics/03_conversionOperartion.js
@@ -24,7 +24,7 @@ let var4 = Number(var3);
 
 let val5 = undefined;
 let val6 = Number(val5);
-console.log(val6);//NaN
+// console.log(val6);//NaN
 
 
 
@@ -50,9 +50,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);//
-console.log(typeof stringNumber);
+// console.log(stringNumber);//
+// console.log(typeof stringNumber);
 
+// *********************** Operations ***********************
 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);//1
+// console.log(+"");//0
+console.log(2-"2");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
  
