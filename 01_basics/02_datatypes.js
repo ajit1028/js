a503135a29a2524ee0bc77ba48c7b12ce745e2d1
AJIT SINGH <96468828+ajit1028@users.noreply.github.com>
2024-07-12 08:24:45 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+//alert( 3 + 3) //we are using nodejs not browser
+
+//code reusability should be high
+
+let name  = "bholu"
+let age = 24
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefind
+//symbol => unique
+
+
+// object
+
+console.log(typeof name);//string
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefind);//undefind
